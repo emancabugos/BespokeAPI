5a4238198d18716688ad302f8d495daf568631e3
Arcadier <Arcadier@192.168.254.103>
2020-07-14 08:16:02 +0800

    update Custom Field

--- Object Repository/ADMIN/Custom Item Fields Page/a_Checkbox/textfield_option1.rs
+++ Object Repository/ADMIN/Custom Item Fields Page/a_Checkbox/textfield_option1.rs
@@ -6,12 +6,12 @@
    <elementGuidId>2a438be3-032b-456a-805e-c7b67345ab8a</elementGuidId>
    <selectorCollection>
       <entry>
-         <key>XPATH</key>
-         <value>//div[3]/div[2]/div[4]/div/div[1]/div[2]/div/form/div/div[8]/div/ul/li[1]/div/div[2]/input</value>
+         <key>BASIC</key>
+         <value>/html/body/div[3]/div[2]/div[3]/div/div[1]/div[2]/div/form/div/div[9]/div/ul/li[1]/div/div[2]/input[count(. | //input[@type = 'text']) = count(//input[@type = 'text'])]</value>
       </entry>
       <entry>
-         <key>BASIC</key>
-         <value>/html/body/div[3]/div[2]/div[3]/div/div[1]/div[2]/div/form/div/div[8]/div/ul/li[1]/div/div[2]/input</value>
+         <key>XPATH</key>
+         <value>//div[3]/div[2]/div[4]/div/div[1]/div[2]/div/form/div/div[8]/div/ul/li[1]/div/div[2]/input</value>
       </entry>
    </selectorCollection>
    <selectorMethod>BASIC</selectorMethod>
@@ -21,6 +21,20 @@
       <matchCondition>equals</matchCondition>
       <name>xpath</name>
       <type>Main</type>
-      <value>/html/body/div[3]/div[2]/div[3]/div/div[1]/div[2]/div/form/div/div[8]/div/ul/li[1]/div/div[2]/input</value>
+      <value>/html/body/div[3]/div[2]/div[3]/div/div[1]/div[2]/div/form/div/div[9]/div/ul/li[1]/div/div[2]/input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>input</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>text</value>
    </webElementProperties>
 </WebElementEntity>
