53eeec9a44f09557b379df90eaf288c3b850d92a
arcadier01 <arcadier01@arcadier01-PC>
2021-03-09 08:54:44 +0800

    conflict

--- Test Suites/SuperBaby/Smoke Testing B2C/DAY_02.ts
+++ Test Suites/SuperBaby/Smoke Testing B2C/DAY_02.ts
@@ -9,7 +9,7 @@
       <TestSuiteRunConfiguration>
          <configuration>
             <groupName>Web Desktop</groupName>
-            <profileName>default</profileName>
+            <profileName>URL &amp; Credentials</profileName>
             <runConfigurationId>Chrome</runConfigurationId>
          </configuration>
          <runEnabled>true</runEnabled>
